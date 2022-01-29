c048f87e3e33fecd1b469adcc1170134a0eefba9
Lakshmi Narayana <63528587+narayana9999@users.noreply.github.com>
2022-01-29 18:07:38 +0530

    Add files via upload

---
+++ numbertype.js
@@ -0,0 +1,11 @@
+number =-89;
+
+if (number > 0) {
+    console.log("The number is positive");
+}
+else if (number == 0) {
+  console.log("The number is zero");
+}
+else {
+     console.log("The number is negative");
+}
